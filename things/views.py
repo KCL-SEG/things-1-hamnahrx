442a6c90eb6a5e45cd086e620e82cd053e15f753
Hamnah <hamnah.1.rassen@kcl.ac.uk>
2023-12-06 17:51:15 +0000

    Update views.py

--- things/views.py
+++ things/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render
 
 def home(request):
-    html = "<html><head><title>Things</title><body><h1>Things</h1></body></head></html>"
-    return HttpResponse(html)
+    return render(request, 'things/home.html')
