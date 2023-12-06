f0b46f4b4833b33d55746435d43a430e55496e75
Hamnah <hamnah.1.rassen@kcl.ac.uk>
2023-12-06 18:05:33 +0000

    Refine Thing model

--- things/models.py
+++ things/models.py
@@ -1,6 +1,13 @@
 from django.db import models
 
+def validate_quantity_range(value):
+    if value < 0 or value > 100:
+        raise models.ValidationError('Quantity must be between 0 and 100.')
+
 class Thing(models.Model):
-    name = models.CharField(max_length=255)
-    description = models.TextField()
-    quantity = models.IntegerField()
+    name = models.CharField(max_length=30, unique=True, blank=False)
+    description = models.TextField(max_length=120, blank=True)
+    quantity = models.IntegerField(validators=[validate_quantity_range])
+
+    def __str__(self):
+        return self.name
