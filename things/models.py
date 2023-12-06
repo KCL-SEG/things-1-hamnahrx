6d5457fdb23209bc82a1f7d5b9c1adf70409b6e0
Hamnah <hamnah.1.rassen@kcl.ac.uk>
2023-12-06 18:01:05 +0000

    Make migrations

--- things/models.py
+++ things/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
 class Thing(models.Model):
-    name = models.CharField(max_length=30, unique=True, blank=False)
-    description = models.CharField(max_length=120, blank=True)
-    quantity = models.IntegerField(default=0, choices=[(i, i) for i in range(101)])
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    quantity = models.IntegerField()
